36a7733247ee9474a320321515fc9638af231002
Justin Schroeder <justin@wearebraid.com>
2020-02-27 23:59:14 -0500

    Removes authentication password

--- index.js
+++
@@ -1,9 +0,0 @@
-const protect = require('static-auth')
-const route = '/'
-const { USERNAME, PASSWORD } = process.env
-const isAuthenticated = (user, pass) => (user === USERNAME && pass === PASSWORD)
-const options = {
-  directory: __dirname + '/public'
-}
-const app = protect(route, isAuthenticated, options)
-module.exports = app
