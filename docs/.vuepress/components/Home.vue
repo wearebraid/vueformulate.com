19e4b40850d075f1f7575f3a34192c445550fbe5
Andrew Boyd <andrew@wearebraid.com>
2020-03-02 10:36:41 -0500

    homepage layout revisions

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -10,32 +10,32 @@
         :alt="data.heroAlt || 'hero'"
       >
 
-      <h1
-        v-if="data.heroText !== null"
-        id="main-title"
-      >
-        {{ data.heroText || $title || 'Hello' }}
-      </h1>
+      <div class="hero-meta">
+        <h1
+          v-if="data.heroText !== null"
+          id="main-title"
+        >
+          {{ data.heroText || $title || 'Hello' }}
+        </h1>
 
-      <p
-        v-if="data.tagline !== null"
-        class="description"
-      >
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-      </p>
+        <p
+          v-if="data.tagline !== null"
+          class="description"
+        >
+          {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+        </p>
 
-      <p
-        v-if="data.actionText && data.actionLink"
-        class="action"
-      >
-        <NavLink
-          class="action-button"
-          :item="actionLink"
-        />
-      </p>
+        <p
+          v-if="data.actionText && data.actionLink"
+          class="action"
+        >
+          <NavLink
+            class="action-button"
+            :item="actionLink"
+          />
+        </p>
+      </div>
     </header>
-
-    <Content class="theme-default-content custom" />
 
     <div
       v-if="data.features && data.features.length"
@@ -48,6 +48,45 @@
       >
         <h2>{{ feature.title }}</h2>
         <p>{{ feature.details }}</p>
+      </div>
+
+    </div>
+
+    <hr>
+
+    <Content class="theme-default-content custom" />
+
+    <div class="hero">
+      <img
+        v-if="data.heroImage"
+        :src="$withBase(data.heroImage)"
+        :alt="data.heroAlt || 'hero'"
+      >
+
+      <div class="hero-meta">
+        <h1
+          v-if="data.heroText !== null"
+          id="main-title"
+        >
+          {{ data.heroText || $title || 'Hello' }}
+        </h1>
+
+        <p
+          v-if="data.tagline !== null"
+          class="description"
+        >
+          {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+        </p>
+
+        <p
+          v-if="data.actionText && data.actionLink"
+          class="action"
+        >
+          <NavLink
+            class="action-button"
+            :item="actionLink"
+          />
+        </p>
       </div>
     </div>
 
@@ -87,22 +126,35 @@ export default {
   max-width $homePageWidth
   margin 0px auto
   display block
+  header.hero
+    padding 8em 0 3em 0
   .hero
-    text-align center
+    display flex
+    align-items center
+    justify-content center
+    text-align left
+    padding 5em 0 0 0
     img
-      max-width: 100%
-      max-height 280px
+      width: 100%
+      max-width: 130px
       display block
-      margin 3rem auto 1.5rem
+      margin-right 5em
+      margin-top 0.5em
+    .hero-meta
+      margin 0
     h1
       font-size 3rem
-    h1, .description, .action
-      margin 1.8rem auto
+      margin 0 0 0.15em 0
+      line-height 1
     .description
       max-width 35rem
       font-size 1.6rem
       line-height 1.3
+      margin 0 auto 2rem auto
       color lighten($textColor, 40%)
+    .action
+      margin: 0
+      line-height 0
     .action-button
       display inline-block
       font-size 1.2rem
@@ -110,6 +162,7 @@ export default {
       background-color $accentColor
       padding 0.8rem 1.6rem
       border-radius 4px
+      line-height 1.7
       transition background-color .1s ease
       box-sizing border-box
       border-bottom 1px solid darken($accentColor, 10%)
@@ -133,11 +186,11 @@ export default {
       pre
         background-color #d6dce6
   .theme-default-content
-    margin-top 5em
+    margin-top 4em
   .features
     border-top 1px solid $borderColor
     padding 1.2rem 0
-    margin-top 2.5rem
+    margin-top 4.5rem
     display flex
     flex-wrap wrap
     align-items flex-start
@@ -156,10 +209,46 @@ export default {
     p
       color lighten($textColor, 25%)
   .footer
+    margin-top 3em
     padding 2.5rem
-    border-top 1px solid $borderColor
     text-align center
     color lighten($textColor, 25%)
+
+@media (max-width: 419px)
+  .home
+    .preview-code
+      border 1px solid #ccc
+      border-radius: 5px
+      margin: 0 -1em 4em -1em
+      .editor
+        margin: 0
+        width: 100%
+        overflow auto
+      .preview
+        width: 100%
+        overflow auto
+
+@media (max-width: 568px)
+  .home
+    header.hero
+      padding 3em 0 2em 0
+    .hero
+      flex-direction column
+      text-align center
+      img
+        margin-right 0
+        margin-bottom 2em
+    .preview-code
+      display: flex
+      flex-direction column
+      .preview
+        order: 2
+        width: 100%
+        overflow auto
+      .editor
+        order: 1
+        width: 100%
+        overflow auto
 
 @media (max-width: $MQMobile)
   .home
--- docs/README.md
+++ docs/README.md
@@ -20,7 +20,7 @@ features:
 - title: 🎨 Bring your own styles
   details: Vue Formulate has simple semantic classes that make styling inputs a snap. Roll your own styles or choose one of our pre-baked themes.
 ---
-
+# Live Code Examples
 
 ### Form validation
 Vue Formulate includes [18 rules](/guide/validation/) and support for defining your own.
