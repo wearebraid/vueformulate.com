58e2c30bc610af7b7e3f08c32a13e07a34df68ce
Justin Schroeder <justin@wearebraid.com>
2020-03-06 22:20:36 -0500

    Adds a why not? section

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,13 +3,15 @@
  * https://vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 import VueFormulate from '@braid/vue-formulate'
+import { de } from '@braid/vue-formulate-i18n'
 import pageComponents from '@internal/page-components'
 import Autocomplete from './Autocomplete'
 import '../../node_modules/@braid/vue-formulate/themes/snow/snow.scss'
 
 export default ({ Vue }) => {
   Vue.use(VueFormulate, {
-    plugins: [ Autocomplete ]
+    plugins: [ Autocomplete, de ],
+    locale: 'de'
   })
   for (const [name, component] of Object.entries(pageComponents)) {
     Vue.component(name, component)
