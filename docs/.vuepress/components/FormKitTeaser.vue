b2ddc11f658cf7a6260654684ddffa67be1ba7b7
Andrew Boyd <andrew@formkit.com>
2023-03-27 10:55:04 -0400

    updates formkit teaser to look less like a 3rd-party ad

--- docs/.vuepress/components/FormKitTeaser.vue
+++ docs/.vuepress/components/FormKitTeaser.vue
@@ -1,105 +1,130 @@
 <template>
-  <div
-    v-if="!dismissed"
-    class="the-formkit-teaser"
-    ref="banner"
-  >
-    <div class="the-teaser-title">
-      <FormKitLogo />
-      <ProBadge class="pro-badge"/>
-    </div>
-    <div class="the-cta">
-      <p>FormKit just got a lot more powerful! <strong>Join FormKit Pro Early Access now.</strong></p>
-      <a href="https://www.formkit.com/pro" class="fk-button">
-        Get Started!
-        <svg viewBox="0 0 14 12" fill="none" data-v-21a21f10=""><path d="M1.66675 6H12.3334" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7.66675 1.33301L12.3334 5.99968L7.66675 10.6663" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-      </a>
-      <span
-        @click="handleDismiss"
-        class="dismiss"
-      >
-        dismiss
-      </span>
+  <div v-if="!dismissed" class="the-formkit-teaser" ref="banner">
+    <div class="container">
+      <div class="the-teaser-title">
+        <a href="https://www.formkit.com">
+          <FormKitLogo />
+        </a>
+      </div>
+      <div class="the-cta">
+        <p>
+          FormKit is the free & open-source successor to Vue Formulate.<br />
+          <strong>Vue 3 support, advanced schema, pro inputs, and more!</strong>
+        </p>
+        <a href="https://www.formkit.com" class="fk-button">
+          Try FormKit!
+          <svg viewBox="0 0 14 12" fill="none" data-v-21a21f10="">
+            <path
+              d="M1.66675 6H12.3334"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M7.66675 1.33301L12.3334 5.99968L7.66675 10.6663"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </a>
+        <span @click="handleDismiss" class="dismiss"> dismiss </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       dismissed: true,
-      height: 0
-    }
+      height: 0,
+    };
   },
   watch: {
-    height () {
+    height() {
       if (this.height) {
-        this.setHeight()
+        this.setHeight();
       }
-    }
+    },
   },
   methods: {
-    handleDismiss () {
-      this.hideBanner()
-      this.$cookies.set('vf_fk_public_notice_dismissed', true, '7d')
+    handleDismiss() {
+      this.hideBanner();
+      this.$cookies.set("vf_fk_public_notice_dismissed", true, "7d");
     },
-    hideBanner () {
-      this.dismissed = true
-      this.height = 0
-      this.setHeight(true)
+    hideBanner() {
+      this.dismissed = true;
+      this.height = 0;
+      this.setHeight(true);
     },
-    getBannerHeight () {
+    getBannerHeight() {
       if (this.$refs && this.$refs.banner) {
-        this.height = Math.ceil(this.$refs.banner.getBoundingClientRect().height)
+        this.height = Math.ceil(
+          this.$refs.banner.getBoundingClientRect().height
+        );
       }
     },
-    setHeight (force) {
-      if ((this.height || force) && typeof document !== 'undefined') {
-        const main = document.querySelectorAll('.theme-container > main')[0]
-        const sidebar = document.querySelectorAll('.theme-container > aside')[0]
+    setHeight(force) {
+      if ((this.height || force) && typeof document !== "undefined") {
+        const main = document.querySelectorAll(".theme-container > main")[0];
+        const sidebar = document.querySelectorAll(
+          ".theme-container > aside"
+        )[0];
 
-        main.style.marginTop = `${this.height}px`
-        sidebar.style.marginTop = `${this.height}px`
+        main.style.marginTop = `${this.height}px`;
+        sidebar.style.marginTop = `${this.height}px`;
       } else {
         setTimeout(() => {
-          this.getBannerHeight()
-          this.setHeight()
-        }, 100)
+          this.getBannerHeight();
+          this.setHeight();
+        }, 100);
       }
-    }
+    },
   },
-  mounted () {
-    this.dismissed = this.$cookies.get('vf_fk_public_notice_dismissed')
+  mounted() {
+    this.dismissed = this.$cookies.get("vf_fk_public_notice_dismissed");
     if (this.dismissed) {
-      this.hideBanner()
+      this.hideBanner();
     } else {
-      window.addEventListener('resize', this.getBannerHeight)
-      this.setHeight()
+      window.addEventListener("resize", this.getBannerHeight);
+      this.setHeight();
     }
   },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.getBannerHeight)
-  }
-}
+  beforeDestroy() {
+    window.removeEventListener("resize", this.getBannerHeight);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .the-formkit-teaser {
-  background: linear-gradient(90deg, #111111 7.73%, #2B2B35 93.88%);
+  background: linear-gradient(-20deg, #141028 7.73%, #373741 93.88%);
   background-size: cover;
   background-repeat: no-repeat;
   color: #fff;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1.5rem 1.5rem;
-  flex-direction: column;
+  padding: 3rem 1.5rem;
   width: 100%;
   box-sizing: border-box;
 
-  @media (min-width: 800px) {
-    flex-direction: row;
+  .container {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    @media (min-width: 600px) {
+      flex-direction: row;
+    }
   }
 }
 
@@ -108,14 +133,21 @@ export default {
   flex-grow: 1;
   align-items: center;
   flex-direction: row;
-  width: 100%;
   margin-bottom: 1em;
+  margin-top: 0.5em;
+  margin-right: 3em;
 
   @media (min-width: 800px) {
     margin-bottom: 0;
-    width: 40%;
+    width: auto;
+    flex-grow: unset;
   }
   @media (min-width: 1000px) {
+  }
+
+  a {
+    display: block;
+    margin-top: 5px;
   }
 
   svg {
@@ -158,31 +190,24 @@ export default {
   }
   @media (min-width: 1000px) {
     flex-wrap: nowrap;
-    width: 60%;
+    width: auto;
+    flex-grow: unset;
   }
 
   p {
-    margin: 0;
+    margin: 0 2em 0 0;
     font-size: 0.85em;
     margin-bottom: 0.75em;
     width: 100%;
-
-    strong::before {
-      content: "\a";
-      white-space: pre;
-    }
 
     @media (min-width: 600px) {
       margin-bottom: 0;
     }
     @media (min-width: 800px) {
       width: auto;
-      margin-right: 0.75em;
-
     }
     @media (min-width: 1000px) {
       font-size: 0.95em;
-      margin-right: 2em;
       margin-bottom: 0;
 
       strong::before {
@@ -195,7 +220,7 @@ export default {
 
 .fk-button {
   padding: 0.75em 1.5em;
-  background: linear-gradient(90deg, #F6F6F6 7.73%, #FFFFFF 93.88%);
+  background: linear-gradient(90deg, #f6f6f6 7.73%, #ffffff 93.88%);
   color: #111111;
   font-weight: bold;
   display: flex;
@@ -213,6 +238,8 @@ export default {
 
 .dismiss {
   margin-left: 2rem;
+  margin-top: 1em;
+  margin-bottom: 1em;
   font-size: 0.85em;
   opacity: 0.66;
   text-decoration: underline;
